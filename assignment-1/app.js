21975f7d199213c4f04f1c454f4c1bf1a87fb5d4
Wlodzimierz Reif <wlodzimierzreif@gmail.com>
2022-05-27 06:50:27 +0100

    first assignment

--- assignment-1/app.js
+++ assignment-1/app.js
@@ -1,17 +1,37 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  //   console.log("server is running");
   const url = req.url;
-  const method = req.method;
   if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
+    res.write("<head><title>Users page</title></head>");
     res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button>Submit</button></form></body>"
+      "<body><form action='/create-user' method='POST'><input type='text' name='username'><button type='submit'>Create user</button></form></body>"
     );
     res.write("</html>");
     return res.end();
+  }
+  if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Users Page</title></html>");
+    res.write("<body><ul><li>User1</li><li>User2</li></ul></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/create-user") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody.split("=")[1]);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    res.end();
   }
 });
 
